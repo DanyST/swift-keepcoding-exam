7c1551668c7394426c5e37b3431ef45054130194
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-07-08 23:33:44 -0400

    Add HotelReservationManagerTests+CancelReservation

---
+++ PracticaSwift.playground/Sources/Tests/HotelReservationManagerTests+CancelReservation.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+extension HotelReservationManagerTests {
+    public func testCancelReservation_WithCorrectId_ReservationCancelledSuccesfully() {
+        let hotelReservationManager = HotelReservationManager()
+        
+        do {
+            assert(hotelReservationManager.allReservations.count == .zero)
+            
+            let clientList = [
+                Client(name: "Goku", age: 40, height: 180),
+                Client(name: "Krilin", age: 40, height: 160),
+            ]
+            let reservationAdded1 = try hotelReservationManager.addReservation(clientList: clientList, durationOfDays: 2, breakfastOption: true)
+            assert(hotelReservationManager.allReservations.count == 1)
+            
+            try hotelReservationManager.cancelReservation(byId: reservationAdded1.id)
+            assert(hotelReservationManager.allReservations.count == .zero)
+            
+            let clientList2 = [
+                Client(name: "Bulma", age: 40, height: 168),
+                Client(name: "Vegeta", age: 40, height: 175),
+            ]
+            let reservationAdded2 = try hotelReservationManager.addReservation(clientList: clientList2, durationOfDays: 1, breakfastOption: true)
+            assert(hotelReservationManager.allReservations.count == 1)
+            
+            try hotelReservationManager.cancelReservation(byId: reservationAdded2.id)
+            assert(hotelReservationManager.allReservations.count == .zero)
+        } catch {
+            assertionFailure("This case must not occur")
+        }
+    }
+    
+    public func testCancelReservation_WithUnknownId_NotFoundError() {
+        let hotelReservationManager = HotelReservationManager()
+        
+        let clientList = [
+            Client(name: "Goku", age: 40, height: 180),
+            Client(name: "Krilin", age: 40, height: 160),
+        ]
+        
+        do {
+            try hotelReservationManager.addReservation(clientList: clientList, durationOfDays: 2, breakfastOption: true)
+            assert(hotelReservationManager.allReservations.count == 1)
+            
+            try hotelReservationManager.cancelReservation(byId: 9289289)
+            assertionFailure("This case must not occur")
+        } catch {
+            assert(hotelReservationManager.allReservations.count == 1)
+            
+            let reservationError = error as? ReservationError
+            assert(reservationError == .notFound)
+        }
+    }
+}
